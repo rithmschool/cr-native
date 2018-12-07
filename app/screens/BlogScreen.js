d02a1e411c33ade8fa64a39799b2115ef9aa41d0
Kevin Welch <kaedub16@gmail.com>
2018-12-07 12:06:48 -0800

    Blog page loads blog post resources and displays a mildly styled list. Also installed NativeBase to experiment with a pre-styled component library

--- app/screens/BlogScreen.js
+++ app/screens/BlogScreen.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Alert, Text, TouchableHighlight, ScrollView, StyleSheet, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 _loadResources = async () => {
   try{
-    const url = 'http://192.168.1.8:3001/blog';
-    console.log('\n~~~~~~~~~~~\nMaking get request to', url);
+    const url = 'http://192.168.1.227:3001/blog';
+    // const url = 'http://192.168.1.8:3001/blog';
     let response = await fetch(url);
     let responseJson = await response.json();
-    console.log('\n~~~~~~~~~~~\nWhich responded with', JSON.stringify(responseJson.blogList.posts[0].title))
-    return responseJson.blogList.posts
+    return responseJson.posts
   } catch (error) {
     console.log('Error:', error)
   }
 }
 
-
-export default class BlogScreen extends React.Component {
+export default class BlogScreen extends Component {
   state = {
     loading: true
   }
+
   static navigationOptions = {
-    title: 'Links'
+    title: 'Blog Posts'
   };
 
   componentDidMount() {
@@ -50,15 +49,29 @@ export default class BlogScreen extends React.Component {
     if (this.state.loading) return <Text>Loading...</Text>
     return (
       <ScrollView style={styles.container}>
-        <Text>{JSON.stringify(Object.keys(this.state.posts[0]))}</Text>
         {this.state.posts.map(post => {
+          const date = new Date(post.created_at);
+          const formattedDate = date.toDateString();
           return (
-          <TouchableHighlight key={post.id} onPress={this._handleButton} style={styles.cardContainer}>
-          <View style={styles.card}>
-            <Text>Title: {post.title}</Text>
-            <Text>Created At:{post.created_at}</Text>
+          <View  key={post.id}>
+            <TouchableHighlight onPress={this._handleButton} style={styles.cardContainer}>
+              <View style={styles.card}>
+                <Text style={styles.cardTitle}>{post.title}</Text>
+                <Text>By
+                  <Text > {post.author} </Text> 
+                  <Text> • </Text>
+                  <Text> {formattedDate}</Text>
+                </Text>
+              </View>
+            </TouchableHighlight>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+              }}
+            />
           </View>
-          </TouchableHighlight>)
+          )
         })}
       </ScrollView>
     );
@@ -68,18 +81,20 @@ export default class BlogScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff'
   },
+  cardTitle: {
+    fontSize: 18,
+    paddingVertical: 5
+  },
   card: {
-    textAlign: 'center',
     padding: 10,
+    textAlign: 'center',
     width: "100%",
     height: 'auto',
     backgroundColor: 'lightgrey'
   },
   cardContainer: {
-    marginVertical: 10,
     width: "100%",
     height: 'auto',
   },
